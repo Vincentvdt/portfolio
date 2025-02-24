16dd00d823ddf74c8dc11406129d370675cb74a1
Vincent <51759317+Vincentvdt@users.noreply.github.com>
2025-02-24 15:53:21 +0100

    Initial commit

---
+++ prettier.config.mjs
@@ -0,0 +1,17 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 80, // Max line length before wrapping
+  tabWidth: 2, // Number of spaces per indentation level
+  useTabs: false, // Use spaces instead of tabs
+  semi: false, // Omit semicolons
+  singleQuote: true, // Use single quotes instead of double quotes
+  trailingComma: 'es5', // Add trailing commas where valid in ES5
+  bracketSpacing: true, // Print spaces between brackets in object literals
+  arrowParens: 'always', // Always include parentheses around arrow function arguments
+  plugins: ['prettier-plugin-tailwindcss'], // Add Tailwind CSS support
+}
+
+export default config
