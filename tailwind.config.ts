121b842be1f3b2054ecfb0d9fd84a093fd8cf322
Vincent <51759317+Vincentvdt@users.noreply.github.com>
2025-03-03 15:11:07 +0100

    add custom spacing scale in rem (#4)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,6 +21,19 @@ export default {
         ntEpika: ['var(--font-nt-epika)'],
         nohemi: ['var(--font-nohemi)']
       },
+      spacing: {
+        "0.5": "0.25rem", // 4px
+        "1": "0.5rem",    // 8px
+        "2": "1rem",      // 16px
+        "3": "1.5rem",    // 24px
+        "4": "2rem",      // 32px
+        "5": "2.5rem",    // 40px
+        "6": "3rem",      // 48px
+        "7": "3.5rem",    // 56px
+        "8": "4rem",      // 64px
+        "9": "4.5rem",    // 72px
+        "10": "5rem",     // 80px
+      }
     },
   },
   plugins: [],
