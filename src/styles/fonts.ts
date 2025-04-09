fe60244e4347f9b2ac5807c8b05312a96e4b7285
vincentvdt <vincent.vidot3@gmail.com>
2025-04-09 23:16:38 +0200

    optimize fonts again

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,77 +1,43 @@
 import localFont from 'next/font/local'
 
-const nohemi = localFont({
+export const nohemi = localFont({
   src: [
-    {
-      path: '../assets/fonts/nohemi/Nohemi-Black.woff2',
-      weight: '900',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/nohemi/Nohemi-ExtraBold.woff2',
-      weight: '800',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/nohemi/Nohemi-Bold.woff2',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/nohemi/Nohemi-SemiBold.woff2',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/nohemi/Nohemi-Medium.woff2',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/nohemi/Nohemi-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/nohemi/Nohemi-Light.woff2',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/nohemi/Nohemi-ExtraLight.woff2',
-      weight: '200',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/nohemi/Nohemi-Thin.woff2',
-      weight: '100',
-      style: 'normal',
-    },
+    { path: '../assets/fonts/nohemi/Nohemi-Thin.woff2', weight: '100' },
+    { path: '../assets/fonts/nohemi/Nohemi-ExtraLight.woff2', weight: '200' },
+    { path: '../assets/fonts/nohemi/Nohemi-Light.woff2', weight: '300' },
+    { path: '../assets/fonts/nohemi/Nohemi-Regular.woff2', weight: '400' },
+    { path: '../assets/fonts/nohemi/Nohemi-Medium.woff2', weight: '500' },
+    { path: '../assets/fonts/nohemi/Nohemi-SemiBold.woff2', weight: '600' },
+    { path: '../assets/fonts/nohemi/Nohemi-Bold.woff2', weight: '700' },
+    { path: '../assets/fonts/nohemi/Nohemi-ExtraBold.woff2', weight: '800' },
+    { path: '../assets/fonts/nohemi/Nohemi-Black.woff2', weight: '900' },
   ],
-  display: 'swap',
   variable: '--font-nohemi',
-  fallback: ['system-ui'],
+  style: 'normal',
+  fallback: ['system-ui', 'Arial'],
   adjustFontFallback: 'Arial',
+  display: 'swap',
+  preload: false,
 })
 
-const franklinGothicHeavy = localFont({
+export const franklinGothicHeavy = localFont({
   src: '../assets/fonts/franklin-gothic/franklin-gothic-heavy.woff2',
+  variable: '--font-franklin-gothic-heavy',
   weight: '600',
   style: 'normal',
-  display: 'swap',
-  variable: '--font-franklin-gothic-heavy',
+  fallback: ['system-ui', 'Arial'],
   adjustFontFallback: 'Arial',
+  display: 'swap',
   preload: false,
 })
 
-const yoppaFude = localFont({
+export const yoppaFude = localFont({
   src: '../assets/fonts/yoppa-fude/yoppa-fude.woff2',
+  variable: '--font-yoppa-fude',
   weight: '400',
   style: 'normal',
-  display: 'swap',
-  variable: '--font-yoppa-fude',
+  fallback: ['system-ui', 'Arial'],
   adjustFontFallback: 'Arial',
+  display: 'swap',
   preload: false,
 })
-
-export { nohemi, franklinGothicHeavy, yoppaFude }
